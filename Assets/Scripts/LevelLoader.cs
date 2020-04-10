d247cf6a2badbc58e9e096d51927229f402680cc
FOQ44441 <sheremet.dennis@gmail.com>
2020-04-10 12:05:14 +0300

    Start Files

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+	[SerializeField] float loadDelayInSeconds;
+
+	private void Start() {
+		StartCoroutine(LoadNextScene());
+	}
+
+	public IEnumerator LoadNextScene() {
+		yield return new WaitForSeconds(loadDelayInSeconds);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
